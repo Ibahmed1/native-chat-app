44fecde5e28740fa495db16340ab7ea58904d96a
Ibahmed1 <ib.ahmed1@outlook.com>
2022-06-13 23:28:09 +0100

    stupid react native

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { DefaultTheme, Provider as PaperProvider, IconButton, Title, Headline, TextInput } from "react-native-paper";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#6ACC01",
+    accent: "yellow",
+    surface: "#FF0000",
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View style={styles.titleWrapper}>
+        <Title>Native Chat</Title>
+      </View>
+      <View style={styles.container}></View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titleWrapper: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 30,
   },
 });

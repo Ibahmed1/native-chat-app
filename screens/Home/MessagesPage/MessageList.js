79f9d0eca8744a699bd5cb36e1018ccc25170d21
Ibahmed1 <ib.ahmed1@outlook.com>
2022-06-30 17:00:13 +0100

    trying to fix memory leak

--- screens/Home/MessagesPage/MessageList.js
+++ screens/Home/MessagesPage/MessageList.js
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "../../../firebaseConfig";
 import MessageCard from "./MessageCard";
 
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 const MessageList = (props) => {
   const [messages, setMessages] = useState(undefined);
   const [user, setUser] = useState(undefined);
   const [docId, setDocId] = useState(undefined);
-
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
 
   const unsub = docId
     ? onSnapshot(doc(db, "chats", docId), (doc) => {
